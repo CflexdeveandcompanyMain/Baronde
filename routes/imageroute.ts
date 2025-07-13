5b15f181fe6246138912e8ba321c6f34b36def86
timosdev99 <timothyisah4@gmail.com>
2025-07-13 12:39:34 +0100

    added auth to image route and fixed typo

--- routes/imageroute.ts
+++ routes/imageroute.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { uploadImage, getImages, getImagesByName, getCategories } from '../controllers/imagecontroller';
+import { authToken } from '../middleware/authtoken';
 import upload from '../utils/multer';
 
 const router = Router();
 
 router.post('/upload', upload.single('image'), uploadImage);
-router.get("name/:name", getImagesByName);
-router.get("categories/:categories", getCategories)
-router.get('/', getImages);
+router.get("/name/:name", authToken, getImagesByName);
+router.get("/categories/:categories",  authToken, getCategories)
+router.get('/', authToken, getImages);
 
 
 export default router;
